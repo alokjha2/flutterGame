d2de44fcca0cd8fcbed8c5388d04ec9c00a22d50
alokjha2 <alokj4702@gmail.com>
2024-09-01 16:24:05 +0530

    added correct & wrong animation

--- lib/presentation/games/quiz/widgets/option.dart
+++ lib/presentation/games/quiz/widgets/option.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:game/presentation/games/quiz/widgets/question_controller.dart';
 import 'package:game/utils/constants.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 
 class Option extends StatelessWidget {
   const Option({
@@ -10,66 +10,68 @@ class Option extends StatelessWidget {
     this.index,
     this.press,
   }) : super(key: key);
+
   final String? text;
   final int? index;
   final VoidCallback? press;
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<QuestionController>(
-        init: QuestionController(),
-        builder: (qnController) {
-          Color getTheRightColor() {
-            if (qnController.isAnswered == true) {
-              if (index == qnController.isAnswered) {
-                return kGreenColor;
-              } 
-              // else if (index == qnController.isAnswered &&
-                  // qnController.selectedAns != qnController.correctAns) {
-                // return kRedColor;
-              // }
-            }
-            return kGrayColor;
-          }
+    return Obx(() {
+      final QuestionController qnController = Get.find<QuestionController>();
 
-          IconData getTheRightIcon() {
-            return getTheRightColor() == kRedColor ? Icons.close : Icons.done;
+      Color getTheRightColor() {
+        if (qnController.isAnswered.value) {
+          if (text == qnController.correctAnswer.value) {
+            return kGreenColor;
+          } else if (text == qnController.selectedAnswer.value) {
+            return kRedColor;
           }
+        }
+        return kGrayColor;
+      }
 
-          return InkWell(
-            onTap: press,
-            child: Container(
-              margin: EdgeInsets.only(top: kDefaultPadding),
-              padding: EdgeInsets.all(kDefaultPadding),
-              decoration: BoxDecoration(
-                border: Border.all(color: getTheRightColor()),
-                borderRadius: BorderRadius.circular(15),
+      IconData getTheRightIcon() {
+        return getTheRightColor() == kRedColor ? Icons.close : Icons.done;
+      }
+
+      return InkWell(
+        onTap: press,
+        child: Container(
+          margin: EdgeInsets.only(top: kDefaultPadding),
+          padding: EdgeInsets.all(kDefaultPadding),
+          decoration: BoxDecoration(
+            border: Border.all(color: getTheRightColor()),
+            borderRadius: BorderRadius.circular(15),
+            color: getTheRightColor() == kGrayColor
+                ? Colors.transparent
+                : getTheRightColor().withOpacity(0.2), // Light background for selected option
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "${index! + 1}. $text",
+                style: TextStyle(color: getTheRightColor(), fontSize: 16),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "${index! + 1}. $text",
-                    style: TextStyle(color: getTheRightColor(), fontSize: 16),
-                  ),
-                  Container(
-                    height: 26,
-                    width: 26,
-                    decoration: BoxDecoration(
-                      color: getTheRightColor() == kGrayColor
-                          ? Colors.transparent
-                          : getTheRightColor(),
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(color: getTheRightColor()),
-                    ),
-                    child: getTheRightColor() == kGrayColor
-                        ? null
-                        : Icon(getTheRightIcon(), size: 16),
-                  )
-                ],
-              ),
-            ),
-          );
-        });
+              Container(
+                height: 26,
+                width: 26,
+                decoration: BoxDecoration(
+                  color: getTheRightColor() == kGrayColor
+                      ? Colors.transparent
+                      : getTheRightColor(),
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(color: getTheRightColor()),
+                ),
+                child: getTheRightColor() == kGrayColor
+                    ? null
+                    : Icon(getTheRightIcon(), size: 16),
+              )
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
--- lib/presentation/games/quiz/widgets/question_controller.dart
+++ lib/presentation/games/quiz/widgets/question_controller.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
 class QuestionController extends GetxController {
-  var questions = [].obs;
+  var questions = <Map<String, dynamic>>[].obs;
   var questionNumber = 1.obs;
   var isAnswered = false.obs;
-  var correctAnswer;
-  var selectedAnswer;
+  var correctAnswer = ''.obs;
+  var selectedAnswer = ''.obs;
   var pageController = PageController();
 
   @override
@@ -19,26 +19,33 @@ class QuestionController extends GetxController {
   }
 
   void _loadQuizQuestions() async {
-    final String response = await rootBundle.loadString('assets/json/data.json');
-    final data = await json.decode(response);
-    questions.assignAll(data);
+    try {
+      final String response = await rootBundle.loadString('assets/json/data.json');
+      final List<dynamic> data = json.decode(response);
+      
+      // Ensure each item in the list is a map
+      final List<Map<String, dynamic>> questionsList = 
+          data.map((item) => item as Map<String, dynamic>).toList();
+      
+      questions.assignAll(questionsList);
+    } catch (e) {
+      print("Error loading quiz questions: $e");
+      // Handle the error (e.g., show an error message to the user)
+    }
   }
 
   void updateTheQnNum(int index) {
     questionNumber.value = index + 1;
+    isAnswered.value = false; // Reset for the new question
+    selectedAnswer.value = ''; // Reset selected answer
   }
 
-  void checkAnswer(int index, dynamic question) {
+  void checkAnswer(int index, Map<String, dynamic> question) {
     isAnswered.value = true;
-    correctAnswer = question['correct_answer'];
-    selectedAnswer = question['options'][index];
+    correctAnswer.value = question['correct_answer'];
+    selectedAnswer.value = question['options'][index];
 
-    if (selectedAnswer == correctAnswer) {
-      // Correct answer logic
-      // You can handle the logic here, like updating scores
-    } else {
-      // Incorrect answer logic
-    }
+    Logger().i("Selected answer: ${selectedAnswer.value}, Correct answer: ${correctAnswer.value}");
 
     // After the answer is selected, move to the next question
     Future.delayed(Duration(seconds: 2), () {
