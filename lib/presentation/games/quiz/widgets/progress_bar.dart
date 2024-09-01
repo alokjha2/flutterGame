ba17022952f1ceb8606a613ff0eefebee40261fd
alokjha2 <alokj4702@gmail.com>
2024-09-01 17:48:45 +0530

    added timer animation

--- lib/presentation/games/quiz/widgets/progress_bar.dart
+++ lib/presentation/games/quiz/widgets/progress_bar.dart
@@ -1,52 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:game/presentation/games/quiz/widgets/question_controller.dart';
 import 'package:game/utils/constants.dart';
 import 'package:get/get.dart';
-import 'package:flutter_svg/svg.dart';
-// import 'package:quiz_app/controllers/question_controller.dart'; // Update with your path
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:game/presentation/games/quiz/widgets/question_controller.dart';
 
 class ProgressBar extends StatelessWidget {
   const ProgressBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 35,
-      decoration: BoxDecoration(
-        border: Border.all(color: Color(0xFF3F4768), width: 3),
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: GetBuilder<QuestionController>(
-        builder: (controller) {
-          return Stack(
+    return GetBuilder<QuestionController>(
+      builder: (controller) {
+        // Determine the color based on time remaining
+        Color progressColor;
+        if (controller.timerValue.value <= 10) {
+          progressColor = Colors.red.shade400; // Change to red if time is <= 10 seconds
+        } else {
+          progressColor = Colors.green.shade400; // Default color
+        }
+
+        return Container(
+          width: double.infinity,
+          height: 35,
+          decoration: BoxDecoration(
+            border: Border.all(color: Color(0xFF3F4768), width: 3),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Stack(
             children: [
-              // LayoutBuilder provide us the available space for the container
+              // LayoutBuilder to calculate the width dynamically
               LayoutBuilder(
-                builder: (context, constraints) => Container(
-                  // width: constraints.maxWidth * (controller!..value),
-                  decoration: BoxDecoration(
-                    gradient: kPrimaryGradient, // Use your gradient
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
+                builder: (context, constraints) {
+                  double progressWidth = constraints.maxWidth * (controller.timerValue.value / 60);
+
+                  return Container(
+                    width: progressWidth,
+                    decoration: BoxDecoration(
+                      color: progressColor, // Set progress bar color
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  );
+                },
               ),
               Positioned.fill(
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text("${controller.timerValue.value} sec"),
-                      SvgPicture.asset("assets/icons/clock.svg"),
+                      Text("${controller.timerValue.value} sec", style: TextStyle(color: Colors.white)),
+                      SvgPicture.asset("assets/icons/clock.svg", color: Colors.white),
                     ],
                   ),
                 ),
               ),
             ],
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
